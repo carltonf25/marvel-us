f1cb81e63e60ab282b92049bf720d3c946da0e11
Carlton Freeman <carltonfreeman@Carltons-MacBook-Pro.local>
2018-05-08 20:57:00 -0400

    Character search module basic functionality

--- characters/init.js
+++ characters/init.js
@@ -2,18 +2,21 @@ $(function() {
 
     console.log('init.js loaded')
     
-    // initialize new SuggestionModule listeners
+    // initialize  new SuggestionModule listeners
     var characterModule = new CharacterModule();
 
-    $('.search-button').click( e => { 
-        let charName = $('.form-control').val();       
-        suggestionModule.searchCharacter(charName);
+    $('.search-button').click( e => {
+        e.preventDefault();
+        let charName = $('.form-control').val();
+        characterModule.setCharacter(charName);
+        characterModule.getCharacterId(charName)
+        .then( () => {
+            characterModule.searchCharacter(characterModule.character.name);
+        }); 
     });
 
     $('.clear-button').click (e => {
         location.reload();
     });
-
-
 
 });
