9624f604b2eae2fe16618e2169be6bead32167a9
Carlton Freeman <carltonfreeman@Carltons-MacBook-Pro.local>
2018-05-10 18:00:40 -0400

    iterate over firebase data

--- saved/init.js
+++ saved/init.js
@@ -28,7 +28,7 @@ $(function() {
                 var childData = childSnapshot.val();
                 console.log(childKey);
                 console.log(childData);
-                for (value in childData) {
+                for (value in childData["user-comics"]) {
                     if (!userData.hasOwnProperty(value)) continue;
                     console.log(value);
                     console.log(userData[value]);
